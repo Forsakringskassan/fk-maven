8c4b4e86bfcc6f2476600a83beafb548af0bcb0d
Tomas Bjerre <tomas.bjerre85@gmail.com>
2025-10-12 08:02:48 +0200

    feat: use Spotless plugin with code standard from jar

---
+++ build-and-test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+set -ex
+
+scriptdir=$(pwd)
+
+rm -rf ~/.m2/repository/se/fk
+find . -name target | xargs rm -rf
+
+./mvnw install
+parentVersion=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+
+subprojects=(
+"test-example|./mvnw -s settings.xml spotless:apply"
+)
+
+run_subproject() {
+    local name="$1"
+    local cmds="$2"
+    local testitem="examples/$name"
+
+    echo
+    echo "|"
+    echo "| Testing $testitem"
+    echo "|"
+
+    pushd "$testitem" >/dev/null
+
+    cp -r "$scriptdir/.mvn" .
+    cp "$scriptdir/mvnw" .
+    cp "$scriptdir/mvnw.cmd" .
+    cp "$scriptdir/settings.xml" .
+
+    while IFS= read -r c || [ -n "$c" ]; do
+        c=$(echo "$c" | xargs)
+        if [ -n "$c" ]; then
+            echo "| Running: $c"
+            sed -i "s|<version>latest-SNAPSHOT</version>|<version>$parentVersion</version>|" pom.xml
+
+            bash -c "$c"
+            echo
+        fi
+    done <<< "$cmds"
+
+    popd >/dev/null
+}
+
+for entry in "${subprojects[@]}"; do
+    IFS="|" read -r sub cmd <<< "$entry"
+    run_subproject "$sub" "$cmd"
+done
+
+git diff | cat
+
+git checkout HEAD examples
+
+echo "OK"
+echo
